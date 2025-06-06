9e1478712e62e9124fb18769928431c80dd9ce57
Alexander Delisle <alexanderdelisle@yahoo.ca>
2025-06-06 05:39:42 +0200

    Changed type for new form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@
 
 import { usePlantContext } from "@/components/PlantContext";
 
+export type Frequency =
+  | { type: "daily" }
+  | { type: "multiple-weekly"; days: string[] }
+  | { type: "every-week"; interval: number }
+  | { type: "every-month"; interval: number };
+
 export type Plant = {
   id: number;
   plant_name: string;
-  frequency: number;
+  frequency: Frequency;
   last_watered: string | null;
   sort_order: number;
 };
