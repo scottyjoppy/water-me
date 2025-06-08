a8cbc0681be91bca20e35b68b8f7803356940d4e
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-06-08 11:23:30 +0200

    navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,25 +9,31 @@ const Navbar = () => {
   const { formVisible, setFormVisible } = usePlantContext();
   return (
     <>
-      <nav className="fixed w-full bg-white border-10 p-3 grid grid-cols-4">
-        <Link
-          href="/"
-          className="col-span-1 uppercase font-bold text-2xl hover:underline hover:text-black/70 hover:rotate-1 transition-all"
-        >
-          Home
-        </Link>
-        <Link
-          href="/myplants"
-          className="col-span-2 uppercase font-bold text-2xl hover:underline hover:text-black/70 hover:rotate-1 transition-all"
-        >
-          Your Plants
-        </Link>
-        <button
-          onClick={() => setFormVisible(true)}
-          className="col-span-1 uppercase font-bold text-2xl hover:underline hover:text-black/70 hover:rotate-1 transition-all hover:cursor-pointer"
-        >
-          Add Plant
-        </button>
+      <nav className="fixed w-full bg-white border-10 p-3 grid grid-cols-4 ">
+        <div className="col-span-1 uppercase font-bold text-2xl flex justify-center">
+          <Link
+            href="/"
+            className="hover:underline hover:text-black/70 hover:scale-105 transition-all"
+          >
+            Home
+          </Link>
+        </div>
+        <div className="col-span-2 uppercase font-bold text-2xl flex justify-center">
+          <Link
+            href="/myplants"
+            className="hover:underline hover:text-black/70 hover:scale-105 transition-all"
+          >
+            Your Plants
+          </Link>
+        </div>
+        <div className="col-span-1 font-bold text-2xl flex justify-center">
+          <button
+            onClick={() => setFormVisible(true)}
+            className="uppercase hover:underline hover:text-black/70 hover:scale-105 transition-all hover:cursor-pointer"
+          >
+            Add Plant
+          </button>
+        </div>
       </nav>
       <AnimatePresence>{formVisible && <PlantForm />}</AnimatePresence>
     </>
