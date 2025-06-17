b663f6e1dc6484f324f04312f30c1bcae7e1ca60
Alexander Delisle <alexanderdelisle@yahoo.ca>
2025-06-17 05:24:39 +0200

    Update plantlist page.tsx

--- src/app/(protected)/myplants/page.tsx
+++ src/app/(protected)/myplants/page.tsx
@@ -2,7 +2,7 @@ import PlantList from "@/components/PlantList";
 
 const MyPlants = () => {
   return (
-    <section className="h-screen border-10 overflow-y-auto bg-green-600 py-16 px-4 ">
+    <section className="h-[100dvh] border-10 overflow-y-auto bg-green-600 py-16 px-4 ">
       <PlantList />
     </section>
   );
