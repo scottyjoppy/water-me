ab5d24bc846597f2bb6162d4a7f04b3c02f1b2e9
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-07-17 16:56:23 +0200

    Wrote up the email, just need to add the function that calls it with the users info

--- src/emails/welcome.tsx
+++ src/emails/welcome.tsx
@@ -1,5 +1,6 @@
 import { Plant } from "@/types/databaseValues";
 import {
+  Button,
   Heading,
   Html,
   Section,
@@ -14,8 +15,22 @@ interface emailProps {
   plants: Plant[];
 }
 
-export function Email(props: { props: emailProps }) {
-  const user = { name: null };
+export function Email({ name, email, plants }: emailProps) {
+  // export function Email({
+  //   name = "Alexander",
+  //   email = "test@example.com",
+  //   plants = [],
+  // }: emailProps) {
+  //   plants = [
+  //     {
+  //       id: "123",
+  //       plant_name: "Tim",
+  //       frequency: { type: "every-day", interval: 1 },
+  //       last_watered: "2025-07-10",
+  //       sort_order: 10,
+  //       user_id: "123",
+  //     },
+  //   ];
 
   return (
     <Html lang="en">
@@ -24,11 +39,34 @@ export function Email(props: { props: emailProps }) {
           presets: [pixelBasedPreset],
         }}
       >
-        <Section className="border-8 border-black border-solid rounded-xl">
+        <Section className="border-8 border-black border-solid bg-amber-400 rounded-xl p-4">
           <Heading as="h1" className="text-center">
-            Hello {user.name ? user.name : "User"}
+            Hello, {name || "User"}
           </Heading>
-          <Text>What is up</Text>
+          <Heading as="h2" className="italic text-center">
+            Your plants need watering.
+          </Heading>
+          {Array.isArray(plants) && plants.length > 0 ? (
+            <>
+              {plants.map((plant) => (
+                <Text key={plant.id}>
+                  {plant.plant_name} - Last watered {plant.last_watered}
+                </Text>
+              ))}
+              <Section className="flex justify-center">
+                <Button
+                  href="https://water-me-now.vercel.app/myplants"
+                  className="bg-blue-300 border-4 border-solid border-black px-3 py-1 uppercase font-bold rounded-xl text-black"
+                >
+                  Water
+                </Button>
+              </Section>
+            </>
+          ) : (
+            <Section className="text-center my-10">
+              Just kidding no they don't, ignore this email.
+            </Section>
+          )}
         </Section>
       </Tailwind>
     </Html>
