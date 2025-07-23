4fb856d151ea5728a0148e9f7c880eb78b33f320
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-07-23 08:46:02 +0200

    removed verceljson and added auth

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,7 +1,14 @@
 import checkAllUsersPlants from "@/utils/checkAllUsersPlants";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const authHeader = req.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
   console.log("Cron job started at:", new Date().toISOString());
 
   try {
